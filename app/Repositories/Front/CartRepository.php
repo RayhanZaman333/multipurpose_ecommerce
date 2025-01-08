6e3b3f85aef8af938c482db406de3c91db737130
Rayhan Zaman <rayhan.zaman333@gmail.com>
2025-01-08 18:20:24 +0600

    coupon

--- app/Repositories/Front/CartRepository.php
+++ app/Repositories/Front/CartRepository.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Session;
 
 class CartRepository
 {
-
     /**
      * Store cart.
      *
@@ -24,8 +23,6 @@ class CartRepository
 
     public function store($request)
     {
- 
-       
         $qty_check  = 0;
         $input = $request->all();
         $input['option_name']=[];
@@ -34,15 +31,13 @@ class CartRepository
 
         $qty = isset($input['quantity']) ? $input['quantity'] : 1 ;
 
-
-
         $qty = is_numeric($qty) ? $qty : 1;
-
     
-        if($input['options_ids']){
-            foreach(explode(',',$input['options_ids']) as $optionId){
+        if ($input['options_ids']) {
+            foreach (explode(',', $input['options_ids']) as $optionId) {
                 $option = AttributeOption::findOrFail($optionId);
-                if($qty > $option->stock){
+
+                if ($qty > $option->stock) {
                     $data = [ 'message' => 'Product Out Of Stock', 'status' => 'outStock'];
                     return $data;
                 }
@@ -51,7 +46,7 @@ class CartRepository
      
         $cart = Session::get('cart');
 
-        $item = Item::where('id',$input['item_id'])->select('id','name','photo','discount_price','previous_price','slug','item_type','license_name','license_key', 'stock')->first();
+        $item = Item::where('id',$input['item_id'])->select('id', 'name', 'photo', 'discount_price', 'previous_price', 'slug', 'item_type', 'license_name', 'license_key', 'stock')->first();
        
         if($item->item_type == 'normal' ){
             if($item->stock < $request->quantity){
@@ -59,39 +54,36 @@ class CartRepository
                return $data;
            }
         }
-
-        
      
         $single = isset($request->type) ? ($request->type == '1' ? 1 : 0 ) : 0;
 
-        if(Session::has('cart')){
-            if($item->item_type == 'digital' || $item->item_type == 'license'){
-                $check = array_key_exists($input['item_id'],Session::get('cart'));
+        if (Session::has('cart')) {
+            if ($item->item_type == 'digital' || $item->item_type == 'license') {
+                $check = array_key_exists($input['item_id'], Session::get('cart'));
 
-                if($check){
+                if ($check) {
                     $data = [ 'message' => 'Product already added', 'status' => 'alreadyInCart'];
                     return $data;
-                }else{
-                    if(array_key_exists($input['item_id'].'-',Session::get('cart'))){
-                      
+                } else {
+                    if (array_key_exists($input['item_id'].'-', Session::get('cart'))) {
                         $data = [ 'message' => 'Product already added', 'status' => 'alreadyInCart'];
+
                         return $data;
                     }
                 }
             }
-
         }
 
         $option_id = [];
 
-        if($single == 1){
+        if ($single == 1) {
             $attr_name = [];
             $option_name = [];
             $option_price = [];
 
-            if(count($item->attributes) > 0){
-                foreach($item->attributes as $attr){
-                    if(isset($attr->options[0]->name)){
+            if (count($item->attributes) > 0) {
+                foreach ($item->attributes as $attr) {
+                    if (isset($attr->options[0]->name)) {
                         $attr_name[] = $attr->name;
                         $option_name[] = $attr->options[0]->name;
                         $option_price[] = $attr->options[0]->price;
@@ -105,63 +97,61 @@ class CartRepository
             $input['option_name'] = $option_name;
             $input['option_id'] = $option_id;
 
-            if($request->quantity != 'NaN'){
+            if ($request->quantity != 'NaN') {
                 $qty = $request->quantity;
                 $qty_check = 1;
-            }else{
+            } else {
                 $qty = 1;
             }
-
-        }else{
-
-
-            if($input['attribute_ids']){
-                foreach(explode(',',$input['attribute_ids']) as $attrId){
+        } else {
+            if ($input['attribute_ids']) {
+                foreach (explode(',',$input['attribute_ids']) as $attrId) {
                     $attr = Attribute::findOrFail($attrId);
+
                     $attr_name[] = $attr->name;
                 }
+
                 $input['attr_name'] = $attr_name;
             }
 
-            if($input['options_ids']){
-                foreach(explode(',',$input['options_ids']) as $optionId){
+            if ($input['options_ids']) {
+                foreach (explode(',', $input['options_ids']) as $optionId) {
                     $option = AttributeOption::findOrFail($optionId);
+                    
                     $option_name[] = $option->name;
                     $option_price[] = $option->price;
                     $option_id[] = $option->id;
                 }
+
                 $input['option_name'] = $option_name;
                 $input['option_price'] = $option_price;
             }
-
-           
         }
-
-     
-      
 
         if (!$item) {
             abort(404);
         }
 
-
         $option_price = array_sum($input['option_price']);
+
         $attribute['names'] = $input['attr_name'];
         $attribute['option_name'] = $input['option_name'];
 
-        if(isset($request->item_key) && $request->item_key !=(int) 0){
-            $cart_item_key = explode('-',$request->item_key)[1];
-
-        }else{
-            $cart_item_key = str_replace(' ','',implode(',',$attribute['option_name']));
+        if (isset($request->item_key) && $request->item_key != (int) 0) {
+            $cart_item_key = explode('-', $request->item_key)[1];
+        } else {
+            $cart_item_key = str_replace(' ', '', implode(',', $attribute['option_name']));
         }
 
         $attribute['option_price'] = $input['option_price'];
+
         $cart = Session::get('cart');
+
         // if cart is empty then this the first product
         if (!$cart || !isset($cart[$item->id.'-'.$cart_item_key])) {
-            $license_name = json_decode($item->license_name,true);
-            $license_key = json_decode($item->license_name,true);
+            $license_name = json_decode($item->license_name, true);
+            $license_key = json_decode($item->license_name, true);
+
             $cart [$item->id.'-'.$cart_item_key] = [
                     'options_id' => $option_id,
                     'attribute' => $attribute,
@@ -179,73 +169,78 @@ class CartRepository
             ];
 
             Session::put('cart', $cart);
-            $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
-            return $mgs;
 
+            $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
+
+            return $mgs;
         }
 
-   
         // if cart not empty then check if this product exist then increment quantity
         if (isset($cart[$item->id.'-'.$cart_item_key])) {
-          
             $cart = Session::get('cart');
 
-            if($qty_check == 1){
+            if ($qty_check == 1) {
                 $cart[$item->id.'-'.$cart_item_key]['qty'] =  $qty;
-            }else{
+            } else {
                 $cart[$item->id.'-'.$cart_item_key]['qty'] +=  $qty;
             }
 
-            if($item->item_type == 'normal' ){
-          
-                if($item->stock <= (int)$cart[$item->id.'-'.$cart_item_key]['qty']){
+            if ($item->item_type == 'normal' ) {
+                if ($item->stock <= (int)$cart[$item->id.'-'.$cart_item_key]['qty']) {
                     $data = [ 'message' => 'Product Out Of Stock', 'status' => 'outStock'];
                     return $data;
                 }
             }
 
-
             Session::put('cart', $cart);
 
-            if($qty_check == 1){
+            if ($qty_check == 1) {
                 $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
-            }else{
+            } else {
                 $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
             }
 
             $qty_check = 0;
+
             return $mgs;
         }
 
-            $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
-           return $mgs;
+        $mgs = ['message' => __('Product add successfully') , 'qty' => count(Session::get('cart'))];
 
-
+        return $mgs;
     }
 
 	public function promoStore($request)
 	{
-
         $input = $request->all();
+
         $promo_code = PromoCode::where('status', 1)->whereCodeName($input['code'])->where('no_of_times', '>', 0)->first();
 
-
-        if($promo_code){
+        if ($promo_code) {
             $cart = Session::get('cart');
             $cartTotal = PriceHelper::cartTotal($cart, 2);
-            $discount = $this->getDiscount($promo_code->discount,$promo_code->type,$cartTotal);
+
+            $discount = $this->getDiscount($promo_code->discount, $promo_code->type, $cartTotal);
 
             $coupon= [
                 'discount' => $discount['sub'],
                 'code'  => $promo_code
             ];
-            Session::put('coupon',$coupon);
 
-            return [
-                'status'  => true,
-                'message' => __('Promo code found!')
-            ];
-        }else{
+            if (session()->has('coupon')) {
+                return [
+                    'status'  => false,
+                    'message' => __('Coupon code already applied!')
+                ];
+            } else {
+                Session::put('coupon', $coupon);
+
+                return [
+                    'status'  => true,
+                    'message' => __('Coupon code applied!')
+                ];
+            }
+        } else {
             return [
                 'status'  => false,
                 'message' => __('No coupon code found')
@@ -253,20 +248,18 @@ class CartRepository
         }
     }
 
-
-
 	public function getCart()
 	{
         $cart = Session::has('cart') ? Session::get('cart') : null;
-        return $cart;
 
+        return $cart;
     }
 
-    public function getDiscount($discount,$type,$price){
-        if($type == 'amount'){
+    public function getDiscount($discount, $type, $price){
+        if ($type == 'amount') {
             $sub = $discount;
             $total = $price - $sub;
-        }else{
+        } else {
             $val = $price / 100;
             $sub = $val * $discount;
             $total = $price - $sub;
@@ -277,5 +270,4 @@ class CartRepository
             'total' => $total
         ];
     }
-
 }
